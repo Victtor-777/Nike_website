06226447fc5ade9e8728a0d494b2df19ab3e053f
Victtor777 <victtor.eng.software@gmail.com>
2024-04-20 17:24:56 -0300

    Add section Super Quality

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,13 +1,38 @@
 interface ButtonProps {
   label: string;
-  iconURL: string;
+  iconURL?: string;
+  backgroundColor?: string;
+  textColor?: string;
+  borderColor?: string;
+  fullWidth?: boolean;
 }
 
-export function Button({ label, iconURL }: ButtonProps) {
+export function Button({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}: ButtonProps) {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
       {label}
-      <img src={iconURL} className="ml-2 rounded-full w-5 h-5" />
+
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full bg-white w-5 h-5"
+        />
+      )}
     </button>
   );
 }
